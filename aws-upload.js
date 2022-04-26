a92dae1c1d2326809b2365b2bd5eaf279d0898b7
Valentin Oprea <aeroprea@gmail.com>
2022-04-26 20:30:35 +0300

    moved code from server to upload.js page

--- aws-upload.js
+++ aws-upload.js
@@ -1,20 +1,6 @@
 require('dotenv').config();
+const sharp = require('sharp');
 const AWS = require('aws-sdk');
-const { resize } = require('jimp');
-const Jimp = require('jimp');
-
-module.exports.testFunction = (req,res) => {
-   console.log("testing, attention");
-   return new Promise((resolve, reject) => {
-      let a =1;
-      if(a>0){
-         console.log(b);
-         return resolve(1);
-      } else {
-         return reject(0);
-      }
-   });
-}
 
 //Configure your aws!
 const credentials = {
@@ -44,13 +30,6 @@ var bucketParams = {
 
 // Function that uploads object
 module.exports.uploadImage = async (data, fileName, category) => {
-// s3client.listBuckets(function(err, data) {
-//    if (err) {
-//      console.log("Error", err);
-//    } else {
-//      console.log("SUUUUCCCCESSSSS BA", data.Buckets);
-//    }
-//  });
 
    switch (category) {
       case 'image':
@@ -94,9 +73,7 @@ module.exports.uploadImage = async (data, fileName, category) => {
       console.log("File extension is not suitable");
       reject("Extension is not suitable");
       }
-   })
-
-   
+   })   
 }
 
 //Function that shows all images
@@ -152,3 +129,30 @@ module.exports.deleteImage = async (myKey) => {
    })
 }
 
+//This function resizes in two different sizes the images
+module.exports.obtainResized = async (files) => {
+   let size1 = [];
+   let size2 = [];
+   return new Promise( async (resolve, reject) => {
+      size1 = await callMe(files,150,1);
+      size2 = await callMe(files,50,2);
+      let a = [size1, size2]
+      resolve(a);
+   })
+}
+async function callMe(files,dimension,number) {
+      let resized;
+      let size = [];
+      for( let i = 0; i< files.length; i++) {
+         size[i] = Object.assign({}, files[i]);
+         await sharp(size[i].buffer)
+            .resize({ width: dimension, height: dimension, fit: 'fill'})
+            .toBuffer()
+            .then((data) => {resized = data })
+            .catch((err => {console.log(err)}))
+         size[i].buffer = resized;
+         let stringN = number.toString();
+         size[i].originalname = "size"+stringN+size[i].originalname;
+      }
+      return size;
+}
--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const path = require('path');
-// const testFunction = require('./aws-upload');
-const {testFunction, uploadImage, showImages, deleteImage, resizeImage} = require('./aws-upload');
-const Jimp = require('jimp');
+const {uploadImage, showImages, deleteImage, obtainResized} = require('./aws-upload');
 const sharp = require('sharp');
 
 const multer = require('multer');
 
-const upload = multer({}); //in exemplu aveam in paranteze storage:storage
-// const upload = multer({ dest: 'uploads/'});
+const upload = multer({});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,55 +15,12 @@ app.set('views', path.join(__dirname, 'views'));
 //IMAGE SECTION
 app.post('/images/v1/references/id_de_referinta/images', upload.array('image'), async (req,res) => {
    const files = req.files;
-
-   // ALSO POSSIBLE WITH .THEN .CATCH BLOCK
-   // uploadFile(file.buffer, file.originalname)
-   // .then((response) => {
-   //    console.log("a functionat menutzule")
-   //    res.status(200).send(response);
-   // })
-   // .catch((err) => {
-   //    res.status(400).send(err);
-   // })
-   
-   
-//    let a  = await Jimp.read(files[0].buffer)
-//   .then(image => {
-//     // Do stuff with the image.
-//     console.log("da")
-//     image.resize(150, 150).write("test.png");
-//   })
-//   .catch(err => {
-//     // Handle an exception.
-//     console.log(err);
-//   });
-
    let size1 = [];
    let size2 = [];
-   let resized1;
-   let resized2;
+   let results = await obtainResized(files);
+   size1 = results[0];
+   size2 = results[1];
    
-   for (let j = 0; j< files.length; j++){
-      size1[j] = Object.assign({}, files[j]);
-      size2[j] = Object.assign({}, files[j]);
-      
-      await sharp(size1[j].buffer)
-         .resize({ width: 150, height: 150, fit: 'fill'})
-         .toBuffer()
-         .then((data) => {resized1 = data })
-         .catch((err => {console.log(err)}))
-      size1[j].buffer = resized1;
-      size1[j].originalname = "size1"+size1[j].originalname;
-
-      await sharp(size2[j].buffer)
-         .resize({ width: 50, height: 50, fit: 'fill'})
-         .toBuffer()
-         .then((data) => {resized2 = data})
-         .catch((err => {console.log(err)}))   
-      size2[j].buffer = resized2;
-      size2[j].originalname = "size2"+size2[j].originalname;
-   }
-
    try {
       let responseOriginal = [];
       let response1 = [];
@@ -75,9 +29,9 @@ app.post('/images/v1/references/id_de_referinta/images', upload.array('image'), 
          responseOriginal[i] = await uploadImage(files[i].buffer, files[i].originalname, "image");
          response1[i] = await uploadImage(size1[i].buffer, size1[i].originalname, "image");
          response2[i] = await uploadImage(size2[i].buffer, size2[i].originalname, "image");
-         console.log(responseOriginal[i])
-         console.log(response1[i])
-         console.log(response2[i])
+         // console.log(responseOriginal[i])
+         // console.log(response1[i])
+         // console.log(response2[i])
       }
       let response = [responseOriginal, response1, response2]
       res.send(response);
@@ -171,8 +125,6 @@ app.delete('/images/v1/references/id_de_referinta/avatar/delete/:Folder/:initial
    }
 })
 //END OF AVATAR SECTION
-
-
 
 //front page route
 app.get('/', (req,res) => {
